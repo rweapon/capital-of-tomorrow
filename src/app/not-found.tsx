33369412b9e26d9eb2a68199cf987ee7b6611df0
Bogdan_Ishcenko <bogdan.ishchenko.852@gmail.com>
2025-06-17 22:13:31 +0300

    5 commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+
+import Link from 'next/link';
+
+export default function NotFound() {
+
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <h1 className="text-4xl font-bold mb-4">404</h1>
+            <p className="text-xl mb-8">Page Not Found</p>
+
+            <Link href="/" className="mt-4 text-blue-500 hover:underline">
+                Return Home
+            </Link>
+        </div>
+    );
+}
