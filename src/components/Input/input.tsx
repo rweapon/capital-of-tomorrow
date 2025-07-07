dde9eac2319448823a30a7c2a77a0e54d79bb7f9
rweapon <mrkasper67@gmail.com>
2025-07-07 20:30:09 +0300

    feat(A-6): Added 2 and 3 steps to apply page

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -18,7 +18,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
           type={type}
           className={cn(
             type !== 'file'
-              ? 'flex h-11 w-full rounded-2xl bg-primary-foreground  px-3 py-1 text-base transition-colors font-mont placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-lg shadow-inner'
+              ? 'flex h-11 w-full rounded-2xl bg-primary-foreground  px-3 py-1 text-base transition-colors font-mont placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-lg shadow-inner hover:bg-primary-foreground/80'
               : 'hidden',
 
             className
@@ -30,9 +30,9 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
         {type === 'file' && (
           <label
             htmlFor={id}
-            className='w-48 flex items-center justify-center bg-secondary rounded-2xl text-lg font-mont font-semibold text-primary  input-shadow select-none cursor-pointer px-8 py-1 '
+            className='w-48 flex items-center justify-center bg-secondary rounded-2xl text-lg font-mont font-semibold text-primary  input-shadow select-none cursor-pointer px-8 py-1 hover:bg-secondary/80 transition-colors'
           >
-            {}Choose a file
+            Choose a file
           </label>
         )}
       </div>
---
+++ src/views/Apply/Steps/StepTwo.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Input, Textarea } from '@/components';
+
+const StepTwo = () => {
+  return (
+    <div className='flex flex-col items-start justify-between gap-24'>
+      <div className='flex flex-col gap-9 w-full'>
+        <Textarea
+          title='Can you provide a brief overview of your background, including your education, professional or busibusiness experience? (250 words or less) *'
+          id='experience'
+        />
+        <Textarea
+          title='What motivates you to join the event (250 words or less) *'
+          id='motivation'
+        />
+        <Textarea
+          title='Describe your future goals (250 words or less) *'
+          id='future_goals'
+        />
+      </div>
+      <div className='flex flex-col gap-12 w-full'>
+        <h3 className='font-mont font-bold text-4xl text-primary-foreground uppercase self-center'>
+          Additional Information
+        </h3>
+        <Input
+          title='Facebook/LinkedIn Profile link (optional)'
+          id='socials'
+          className='w-1/2'
+        />
+        <Input
+          title='Where did you hear about Capital of Tomorrow? *'
+          id='source'
+          className='w-1/2'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default StepTwo;
