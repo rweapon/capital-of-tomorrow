2a56fb5457493090a6c5bf3e555dfcb6789cf5ad
Ruslan <147932196+rweapon@users.noreply.github.com>
2025-07-09 19:20:53 +0300

    Rename input.tsx to Input.tsx

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
   ({ className, type, title, id, ...props }, ref) => {
     return (
