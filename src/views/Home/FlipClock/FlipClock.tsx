4afd06df2d25947c17a3296185076e51552440ab
rweapon <mrkasper67@gmail.com>
2025-11-29 15:15:22 +0300

    fix(A-27): Исправлен таймер

--- src/views/Home/FlipClock/FlipClock.tsx
+++ src/views/Home/FlipClock/FlipClock.tsx
@@ -1,17 +1,26 @@
 import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import { useTranslations } from 'next-intl';
 import React from 'react';
 
 import '@leenguyen/react-flip-clock-countdown/dist/index.css';
 
 export const FlipClock = () => {
-  const to = new Date('2026-01-01').getTime();
+  const t = useTranslations('home.hero.timer');
+  const to = new Date('2026-01-29').getTime();
+
+  const labels: [string, string, string, string] = [
+    t('one'),
+    t('two'),
+    t('three'),
+    t('four'),
+  ];
   return (
     <FlipClockCountdown
       to={to}
-      daysInHours
-      showLabels={false}
+      showLabels
+      labels={labels}
       showSeparators={false}
-      className='flip-clock !font-monda !font-bold'
+      className='flip-clock !font-monda !font-bold text-[#ffffffc2]'
     />
   );
 };
